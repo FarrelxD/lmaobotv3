f4d68231341206b488a603fa584b8f99f007f580
FarrelxD <76980701+FarrelxD@users.noreply.github.com>
2021-01-14 18:18:37 +0700

    Delete info.java

--- info.java
+++
@@ -1,8 +0,0 @@
-INFO LmaoBott
-Thanks To Allah
-Mhankbarbar
-
-AUTHOR : ARUGAZ
-Modified : KhasaC Kalau ad masalah bisa chat : 082216555691(Jangan spam)
-
-( cd c:\ | cd lmaobott | npm i | npm start )
